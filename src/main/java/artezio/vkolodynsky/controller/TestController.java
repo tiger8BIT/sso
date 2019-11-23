42f8c4d6527c67cb5b15f6cdec1b11634405dcc8
tiger8bit <kolodynskiy.vyacheslav@gmail.com>
2019-11-23 12:16:31 +0300

    thymeleaf test

---
+++ src/main/java/artezio/vkolodynsky/controller/TestController.java
@@ -0,0 +1,14 @@
+package artezio.vkolodynsky.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping
+public class TestController {
+    @GetMapping("/test")
+    public String getTestPage() {
+        return "test";
+    }
+}
